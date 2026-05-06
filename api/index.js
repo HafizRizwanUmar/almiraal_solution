1d355e455307665925eb7aa181f1377d0458ad81
Hafiz Rizwan Umar <teckleoutreach@gmail.com>
2026-05-07 01:03:14 +0500

    commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   res.status(200).json({
     message: "Almiraal Backend is running successfully",
     status: "online",
     time: new Date().toISOString()
   });
-}
+};
