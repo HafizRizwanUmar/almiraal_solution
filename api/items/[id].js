9c13394a466d145ac01e13c07c3efd425acd944a
Hafiz Rizwan Umar <teckleoutreach@gmail.com>
2026-05-08 23:12:08 +0500

    fix: allow DELETE method in [id].js to resolve CORS preflight and delegate to delete handler

--- api/items/[id].js
+++ api/items/[id].js
@@ -3,17 +3,27 @@ const Product = require('../../models/Product');
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') return res.status(200).end();
+  
+  // Get id from URL: /items/:id
+  req.query = req.query || {};
+  if (!req.query.id) {
+    req.query.id = req.url && req.url.split('/').pop().split('?')[0];
+  }
+
+  if (req.method === 'DELETE') {
+    return require('./delete')(req, res);
+  }
+
   if (req.method !== 'GET') return res.status(405).json({ message: 'Method not allowed' });
 
   try {
     await connectDB();
 
-    // Get id from URL: /items/:id
-    const id = req.query.id || (req.url && req.url.split('/').pop().split('?')[0]);
+    const id = req.query.id;
 
     if (!id) return res.status(400).json({ message: 'Product ID required' });
 
